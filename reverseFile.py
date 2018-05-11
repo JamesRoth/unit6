9757ec6b967b629e7e1f3da2ae06ffb6806b4282
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-11 08:38:05 -0400

    Updated from Brython Server: 5/11/2018 8:38:04 AM

--- reverseFile.py
+++ reverseFile.py
@@ -12,4 +12,5 @@ for line in file:
     if len(line)>0:
         for i in range(len(line), 1, -1):
             list1.append(line[i-1])
-    print(list1)
+    for item in list1:
+        print(item)
