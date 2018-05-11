87ca7c8c138ce05efe2f5cb72116cd05a4955717
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-11 08:35:32 -0400

    Updated from Brython Server: 5/11/2018 8:35:31 AM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,14 @@
+#James Roth
+#5/11/18
+#reverseFile.py - user entered file that gets printed backwards
 
+string = input("Enter the name of the file you want to open: ")
+
+file = open(string)
+
+for line in file:
+    list1=[]
+    if len(line)>0:
+        for i in range(len(line), 0, -1):
+            list1.append(line[i])
+    print(list1)
