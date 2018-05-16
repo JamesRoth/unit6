8fc22ff3d66bbca98895a807a9f896f205f71ce1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-16 08:29:47 -0400

    Update palindromes.py

--- palindromes.py
+++ palindromes.py
@@ -4,6 +4,16 @@
 
 file = open("engmix.txt")
 
+def palindrome(text):
+    list1 = []
+    for i in range(0, len(text)):
+        list1.append(text[i])
+    list2 = list1[:]
+    list2.reverse()
+    if list1 == list2:
+        palindromes.append(num)
+
 for item in file:
     item.strip("\n")
     if len(item) > 0:
+        
