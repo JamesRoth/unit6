49e35ef8ec62921a610bb7c3ef07353de75ad5f1
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-17 08:08:31 -0400

    Updated from Brython Server: 5/17/2018 8:08:29 AM

--- wc.py
+++ wc.py
@@ -1,5 +1,5 @@
 #James Roth
 #5/17/18
-#wc.py - takes a file and prints out # of lines, # of words, #of chars
+#wc.py - takes a file and prints out # of lines, # of words, # of chars
 
-
+fileName = input("Enter the name of the file you want: ")
