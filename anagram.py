9d9f7ea02be137928550d2a9cecd6ecb4dfc3b22
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-17 08:40:22 -0400

    Updated from Brython Server: 5/17/2018 8:40:21 AM

--- anagram.py
+++ anagram.py
@@ -9,4 +9,13 @@ list1 = []
 def anagram(string, list1):
     if string in list1:
         print(string)
-    
+
+for item in file:
+    list2 = []
+    for char in item:
+        list2.append(item)
+    list2.sort()
+    word = ""
+    for item in list2:
+        word+=item
+    list1.append(word)
