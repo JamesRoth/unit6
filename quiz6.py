e0cac2ae1d4ffc94025ebfd050eb4a324439a604
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-05-21 08:03:52 -0400

    Updated from Brython Server: 5/21/2018 8:03:51 AM

--- quiz6.py
+++ quiz6.py
@@ -2,5 +2,9 @@
 #5/21/18
 #quiz6.py
 
-thing = "bitconnect"
-print(thing.count("n"))
+file = open("engmix.txt")
+
+def prgm1(char):
+    for line in file:
+        if line.count(char) == 4:
+            print(line)
